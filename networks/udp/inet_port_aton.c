ff8fdd27ee9012f8d41cad02d87e1f6c8167c644
DiSkills <anti.python.fastapi@gmail.com>
2024-11-23 17:04:25 +0300

    Added string conversion to the port

---
+++ networks/udp/inet_port_aton.c
@@ -0,0 +1,30 @@
+#include <limits.h>
+
+#include <arpa/inet.h>
+
+#include "inet_port_aton.h"
+
+
+static int is_digit(char c)
+{
+    return '0' <= c && c <= '9';
+}
+
+
+unsigned short inet_port_aton(const char *s)
+{
+    int port;
+
+    port = 0;
+    for (; *s; s++) {
+        if (!is_digit(*s)) {
+            return 0;
+        }
+        port *= 10;
+        port += *s - '0';
+    }
+    if (port > USHRT_MAX) {
+        return 0;
+    }
+    return htons((unsigned short)port);
+}
---
+++ networks/udp/inet_port_aton.h
@@ -0,0 +1,6 @@
+#ifndef INET_PORT_ATON_H
+#define INET_PORT_ATON_H
+
+unsigned short inet_port_aton(const char *s);
+
+#endif
