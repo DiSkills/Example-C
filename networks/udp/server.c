3dc7e1decd383f43fc7b61ec7cb727e505b2a8e9
DiSkills <anti.python.fastapi@gmail.com>
2024-11-23 19:18:37 +0300

    Added daemonize

--- networks/udp/server.c
+++ networks/udp/server.c
@@ -1,7 +1,9 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <syslog.h>
 
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -35,9 +37,11 @@ static void dgram(int sd)
     res = recvfrom(sd, buf, sizeof(buf), 0,
                    (sockaddr_t *)&client, &client_size);
     if (res == -1) {
-        perror("recvfrom");
+        syslog(LOG_ERR, "recvfrom: %m");
         return;
     }
+    syslog(LOG_INFO, "A datagram was received from %s:%hu",
+           inet_ntoa(client.sin_addr), ntohs(client.sin_port));
     info.requests++;
     info.total += res;
 
@@ -45,9 +49,11 @@ static void dgram(int sd)
     res = sendto(sd, buf, strlen(buf), 0,
                  (sockaddr_t *)&client, client_size);
     if (res == -1) {
-        perror("sendto");
+        syslog(LOG_ERR, "sendto: %m");
         return;
     }
+    syslog(LOG_INFO, "\"%s\" was sent to %s:%hu", buf,
+           inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 }
 
 
@@ -63,14 +69,15 @@ static void run_server(unsigned short port)
 
     sd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (sd == -1) {
-        perror("socket");
+        syslog(LOG_ERR, "socket: %m");
         exit(EXIT_FAILURE);
     }
     res = bind(sd, (sockaddr_t *)&addr, sizeof(addr));
     if (res == -1) {
-        perror("bind");
+        syslog(LOG_ERR, "bind: %m");
         exit(EXIT_FAILURE);
     }
+    syslog(LOG_INFO, "The UDP server was started on port %hu", ntohs(port));
 
     for (;;) {
         dgram(sd);
@@ -80,20 +87,58 @@ static void run_server(unsigned short port)
 }
 
 
+static void reset_leader()
+{
+    pid_t pid;
+
+    pid = fork();
+    if (pid == -1) {
+        syslog(LOG_ERR, "fork: %m");
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+}
+
+
+static void daemonize()
+{
+    close(0);
+    close(1);
+    close(2);
+
+    open("/dev/null", O_RDONLY);
+    open("/dev/null", O_WRONLY);
+    open("/dev/null", O_WRONLY);
+
+    chdir("/");
+
+    reset_leader();
+    setsid();
+    reset_leader();
+}
+
+
 int main(int argc, char **argv)
 {
     unsigned short port;
 
     if (argc != 2) {
         fprintf(stderr, "Expected: %s <port>\n", argv[0]);
-        return 1;
+        exit(EXIT_FAILURE);
     }
-
     port = inet_port_aton(argv[1]);
     if (!port) {
         fprintf(stderr, "An incorrect port was entered\n");
-        return 2;
+        exit(EXIT_FAILURE);
     }
+
+    openlog(argv[0], 0, LOG_USER);
+    daemonize();
+
     run_server(port);
+
+    closelog();
     return 0;
 }
