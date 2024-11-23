0083cfde43cf5ce5a43fa484e96ff907468b1b87
DiSkills <anti.python.fastapi@gmail.com>
2024-11-23 17:59:18 +0300

    Added the reception and processing of the datagram

--- networks/udp/server.c
+++ networks/udp/server.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 
 #include "inet_port_aton.h"
@@ -9,6 +11,43 @@
 
 typedef struct sockaddr sockaddr_t;
 typedef struct sockaddr_in sockaddr_in_t;
+
+enum {
+    BUFFER_SIZE = 1500
+};
+
+typedef struct {
+    unsigned long long requests,
+                       total;
+} info_t;
+
+
+static void dgram(int sd)
+{
+    int res;
+    char buf[BUFFER_SIZE];
+    sockaddr_in_t client;
+    socklen_t client_size;
+    static info_t info = { 0LL, 0LL };
+
+    client_size = sizeof(client);
+    res = recvfrom(sd, buf, sizeof(buf), 0,
+                   (sockaddr_t *)&client, &client_size);
+    if (res == -1) {
+        perror("recvfrom");
+        return;
+    }
+    info.requests++;
+    info.total += res;
+
+    sprintf(buf, "%llu %llu", info.requests, info.total);
+    res = sendto(sd, buf, strlen(buf), 0,
+                 (sockaddr_t *)&client, client_size);
+    if (res == -1) {
+        perror("sendto");
+        return;
+    }
+}
 
 
 int main(int argc, char **argv)
