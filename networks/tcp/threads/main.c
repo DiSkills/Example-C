5bc2f890d2333c532a528e30be850ec3f274a63e
Arkady Sokolenko <github@diskills-developer.org>
2025-03-06 14:38:57 +0300

    added the validation of the args

---
+++ networks/tcp/threads/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    long port;
+    char *endptr;
+
+    if (argc != 2) {
+        fprintf(stderr, "Expected: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    port = strtol(argv[1], &endptr, 10);
+    if (!*argv[1] || *endptr) {
+        fprintf(stderr, "The port is invalid\n");
+        return 2;
+    }
+
+    return 0;
+}
